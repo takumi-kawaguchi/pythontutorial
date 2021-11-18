0b6a1c4b3368d71ae4e2b1eb1fb27228315d19b1
Takumi Kawaguchi <tcgds4@gmail.com>
2021-11-18 23:14:44 +0900

    4章途中まで

--- chapter4/4.py
+++ chapter4/4.py
@@ -18,12 +18,35 @@
 #     print(i)
 
 # range(開始位置, 最大値, 増加量)
-for j in range(5, 10):
-    print(j)
+# for j in range(5, 10):
+#     print(j)
 
-for k in range(0, 10, 3):
-    print(k)
+# for k in range(0, 10, 3):
+#     print(k)
 
-a = ["alice", "bob", "charlie"]
-for i in range(len(a)):
-    print(i, a[i])
+# a = ["alice", "bob", "charlie"]
+# for i in range(len(a)):
+#     print(i, a[i])
+
+# forのelse
+# for n in range(2, 10):
+#     for x in range(2, n):
+#         if n % x == 0:
+#             print(n, 'equals', x, '*', n//x)
+#             break
+#     else:
+#         print(n, 'is a prime number')
+
+# for num in range(2, 10):
+#     if num % 2 == 0:
+#         print('even num:', num)
+#         continue
+#     print('odd num:', num)
+
+def fib(n):
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a + b
+    print()
+fib(2000)
