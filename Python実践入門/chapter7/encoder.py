3e6f2c6a5c80310d22751f27453f4233bfdd647a
Takumi Kawaguchi <tcgds4@gmail.com>
2021-12-01 23:01:56 +0900

    7章途中

--- Python実践入門/chapter7/encoder.py
+++ Python実践入門/chapter7/encoder.py
@@ -1,5 +1,13 @@
 import base64
+import sys
 
 def str_to_base64(x):
     '''convert string to base64 expression'''
     return base64.b64encode(x.encode('utf-8'))
+
+def main():
+    target = sys.argv[1]
+    print(str_to_base64(target))
+
+if __name__ == '__main__':
+    main()
